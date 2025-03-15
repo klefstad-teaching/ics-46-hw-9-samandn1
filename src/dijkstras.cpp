bed24a94f63e46dae087c07853ddcf727744ee99
samandn1 <samandn1@uci.edu>
2025-03-15 10:30:08 +0000

    Switched source and 0

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -1,28 +1,40 @@
 #include "dijkstras.h"
+#include <algorithm>
 
-//const int UNDEFINED = -1;
 
-vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& previous){
+vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& previous) {
+    //cout << "d1\n";
     int numVertices = G.numVertices;
     vector<int> distances(numVertices, INF);
     vector<bool> visited(numVertices, false);
     distances[source] = 0;
     previous[source] = UNDEFINED;
-    //priority_queue<pair<int,int>> minHeap;
+    //priority_queue<pair<int, int>> minHeap;
     priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> minHeap;
     minHeap.push({0, source});
+    //cout << "d2\n";
 
-    while (!minHeap.empty()){
-        // int u = minHeap.
+    while (!minHeap.empty()) {
         int u = minHeap.top().second;
         minHeap.pop();
 
+        // if (u < 0 || u >= numVertices) {
+        //     cerr << "Error: u out of range: " << u << endl;
+        //     return distances;
+        // }
+
         if (visited[u]) continue;
         visited[u] = true;
-        for (const Edge& edge : G[u]){
+        for (const Edge& edge : G[u]) {
             int v = edge.dst;
             int weight = edge.weight;
-            if (!visited[v] && distances[u] + weight < distances[v]){
+
+            // if (v < 0 || v >= numVertices) {
+            //     cerr << "Error: v out of range: " << v << endl;
+            //     return distances;
+            // }
+
+            if (!visited[v] && distances[u] + weight < distances[v]) {
                 distances[v] = distances[u] + weight;
                 previous[v] = u;
                 minHeap.push({distances[v], v});
@@ -31,25 +43,48 @@ vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& prev
     }
     return distances;
 }
-vector<int> extract_shortest_path(const vector<int>& /*distances*/, const vector<int>& previous, int destination){
-    stack<int> pathStack;
+
+
+
+
+
+// vector<int> extract_shortest_path(const vector<int>& /*distances*/, const vector<int>& previous, int destination){
+//     stack<int> pathStack;
+//     vector<int> path;
+
+//     for (int at = destination; at != UNDEFINED; at = previous[at]) {
+//         pathStack.push(at);
+//     }
+
+//     while (!pathStack.empty()) {
+//         path.push_back(pathStack.top());
+//         pathStack.pop();
+//     }
+
+//     return path;
+// }
+
+void print_path(const vector<int>& v, int total) {
+    if (v.empty()) {
+        cout << "Empty Path." << endl;
+        return; 
+    }
+    for (int i = 0; i < total; ++i) {
+        cout << v[i] << " ";
+    }
+    cout << endl;
+}
+
+vector<int> extract_shortest_path(const vector<int>& /*distances*/, const vector<int>& previous, int destination) {
     vector<int> path;
+    int at = destination;
 
-    for (int at = destination; at != UNDEFINED; at = previous[at]) {
-        pathStack.push(at);
+    while (at != -1) {
+        path.push_back(at);
+        at = previous[at];
     }
 
-    while (!pathStack.empty()) {
-        path.push_back(pathStack.top());
-        pathStack.pop();
-    }
-
+    reverse(path.begin(), path.end());
     return path;
 }
-
-void print_path(const vector<int>& v, int total){
-    for(int i=0; i < total; ++i)
-        cout << v[i] << " ";
-}
-
 
--- src/dijkstras_main.cpp
+++ src/dijkstras_main.cpp
@@ -2,11 +2,15 @@
 
 int main() {
     Graph G;
+    //cout << "1\n";
     file_to_graph("src/small.txt", G); // Load graph from file
+    //cout << "2\n";
 
     int source = 0;
     vector<int> previous(G.numVertices, UNDEFINED);
+    //cout << "3\n";
     vector<int> distances = dijkstra_shortest_path(G, source, previous);
+    //cout << "meow\n";
 
     cout << "Shortest distances from node " << source << ":\n";
     for (size_t i = 0; i < distances.size(); i++) {
